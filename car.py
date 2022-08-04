41582104312051b9f79cabe2db8f638498e65217
verybigsnorlax <99829646+verybigsnorlax@users.noreply.github.com>
2022-08-04 03:10:30 -0700

    remove tires

--- car.py
+++ car.py
@@ -4,10 +4,9 @@ from battery.battery import Battery
 from tire.tire import Tire
 
 class Car(Serviceable):
-	def __init__(self, engine : Engine, battery : Battery, tire : Tire):
+	def __init__(self, engine : Engine, battery : Battery):
 		self.engine = engine
 		self.battery = battery
-		self.tire = tire
 
 	def needs_service(self) -> bool:
 		return self.engine.needs_service() or self.battery.needs_service()
