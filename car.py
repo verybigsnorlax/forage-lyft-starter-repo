bc38108160bf62500b3177793f47369190228186
verybigsnorlax <99829646+verybigsnorlax@users.noreply.github.com>
2022-08-04 02:51:13 -0700

    move unit_tset.py

--- car.py
+++ car.py
@@ -1,11 +1,13 @@
 from interface.serviceable import Serviceable
 from engine.engine import Engine
 from battery.battery import Battery
+from tire.tire import Tire
 
 class Car(Serviceable):
-	def __init__(self, engine : Engine, battery : Battery):
+	def __init__(self, engine : Engine, battery : Battery, tire : Tire):
 		self.engine = engine
 		self.battery = battery
+		self.tire = tire
 
 	def needs_service(self) -> bool:
 		return self.engine.needs_service() or self.battery.needs_service()
